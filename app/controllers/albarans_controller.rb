2572754408939b3399619ea8ff4953a6bb11f1cd
Santiago Ramos <sramos@sitiodistinto.net>
2011-02-17 16:51:45 +0000

    Albaranes

--- app/controllers/albarans_controller.rb
+++ app/controllers/albarans_controller.rb
@@ -1,85 +1,31 @@
 class AlbaransController < ApplicationController
-  # GET /albarans
-  # GET /albarans.xml
+
   def index
+    flash[:mensaje] = "Listado de Albaranes de Proveedores" if :seccion == "proveedores"
+    flash[:mensaje] = "Listado de Albaranes de Clientes" if :seccion == "clientes"
+    redirect_to :action => :listado
+  end
+
+  def listado
     @albarans = Albaran.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @albarans }
-    end
   end
 
-  # GET /albarans/1
-  # GET /albarans/1.xml
-  def show
-    @albaran = Albaran.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @albaran }
-    end
+  def editar
+    @albaran = params[:id] ?  Albaran.find(params[:id]) : nil
   end
 
-  # GET /albarans/new
-  # GET /albarans/new.xml
-  def new
-    @albaran = Albaran.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @albaran }
-    end
+  def modificar
+    @albaran = params[:id] ?  Albaran.find(params[:id]) : Albaran.new
+    @albaran.update_attributes params[:albaran]
+    flash[:error] = @albaran
+    redirect_to :action => :listado
   end
 
-  # GET /albarans/1/edit
-  def edit
-    @albaran = Albaran.find(params[:id])
-  end
-
-  # POST /albarans
-  # POST /albarans.xml
-  def create
-    @albaran = Albaran.new(params[:albaran])
-
-    respond_to do |format|
-      if @albaran.save
-        flash[:notice] = 'Albaran was successfully created.'
-        format.html { redirect_to(@albaran) }
-        format.xml  { render :xml => @albaran, :status => :created, :location => @albaran }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @albaran.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /albarans/1
-  # PUT /albarans/1.xml
-  def update
-    @albaran = Albaran.find(params[:id])
-
-    respond_to do |format|
-      if @albaran.update_attributes(params[:albaran])
-        flash[:notice] = 'Albaran was successfully updated.'
-        format.html { redirect_to(@albaran) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @albaran.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /albarans/1
-  # DELETE /albarans/1.xml
-  def destroy
+  def borrar
     @albaran = Albaran.find(params[:id])
     @albaran.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(albarans_url) }
-      format.xml  { head :ok }
-    end
+    flash[:error] = @albaran
+    redirect_to :action => :listado
   end
+
 end
