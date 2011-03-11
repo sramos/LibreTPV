f76ccf28d52368336cb2c4e2d0caeed71cc671d7
Santiago Ramos <sramos@sitiodistinto.net>
2011-03-11 12:38:05 +0000

    Control de clientes y proveedores

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -1,85 +1,31 @@
 class ClientesController < ApplicationController
-  # GET /clientes
-  # GET /clientes.xml
+
   def index
+    flash[:mensaje] = "Listado de Clientes"
+    redirect_to :action => :listado
+  end
+
+  def listado
     @clientes = Cliente.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @clientes }
-    end
   end
 
-  # GET /clientes/1
-  # GET /clientes/1.xml
-  def show
-    @cliente = Cliente.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @cliente }
-    end
+  def editar
+    @cliente = params[:id] ? Cliente.find(params[:id]) : Cliente.new
+    render :partial => "formulario"
   end
 
-  # GET /clientes/new
-  # GET /clientes/new.xml
-  def new
-    @cliente = Cliente.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @cliente }
-    end
+  def modificar
+    cliente = params[:id] ? Cliente.find(params[:id]) : Cliente.new
+    cliente.update_attributes params[:cliente]
+    flash[:error] = cliente
+    redirect_to :action => :listado
   end
 
-  # GET /clientes/1/edit
-  def edit
-    @cliente = Cliente.find(params[:id])
+  def borrar
+    cliente = Cliente.find_by_id params[:id]
+    cliente.destroy
+    flash[:error] = cliente 
+    redirect_to :action => :listado
   end
 
-  # POST /clientes
-  # POST /clientes.xml
-  def create
-    @cliente = Cliente.new(params[:cliente])
-
-    respond_to do |format|
-      if @cliente.save
-        flash[:notice] = 'Cliente was successfully created.'
-        format.html { redirect_to(@cliente) }
-        format.xml  { render :xml => @cliente, :status => :created, :location => @cliente }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @cliente.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /clientes/1
-  # PUT /clientes/1.xml
-  def update
-    @cliente = Cliente.find(params[:id])
-
-    respond_to do |format|
-      if @cliente.update_attributes(params[:cliente])
-        flash[:notice] = 'Cliente was successfully updated.'
-        format.html { redirect_to(@cliente) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @cliente.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /clientes/1
-  # DELETE /clientes/1.xml
-  def destroy
-    @cliente = Cliente.find(params[:id])
-    @cliente.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(clientes_url) }
-      format.xml  { head :ok }
-    end
-  end
 end
--- app/controllers/proveedors_controller.rb
+++ app/controllers/proveedors_controller.rb
@@ -6,26 +6,25 @@ class ProveedorsController < ApplicationController
   end
 
   def listado
-    @proveedors = Proveedor.all
+    @proveedores = Proveedor.all
   end
 
   def editar
-    @proveedor = params[:id] ?  Proveedor.find(params[:id]) : nil
-    @proveedor_familia_id = params[:id] ? @proveedor.familia.id : nil
-    @familias = Familia.all
+    @proveedor = params[:id] ? Proveedor.find(params[:id]) : Proveedor.new
+    render :partial => "formulario"
   end
 
   def modificar
-    @proveedor = params[:id] ?  Proveedor.find(params[:id]) : Proveedor.new
-    @proveedor.update_attributes params[:proveedor]
-    flash[:error] = @proveedor
+    proveedor = params[:id] ? Proveedor.find(params[:id]) : Proveedor.new
+    proveedor.update_attributes params[:proveedor]
+    flash[:error] = proveedor 
     redirect_to :action => :listado
   end
 
   def borrar
-    @proveedor = Proveedor.find(params[:id])
-    @proveedor.destroy
-    flash[:error] = @proveedor
+    proveedor = Proveedor.find_by_id params[:id]
+    proveedor.destroy
+    flash[:error] = proveedor 
     redirect_to :action => :listado
   end
 
