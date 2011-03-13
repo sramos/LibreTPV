c57f4d66b8d0a3e43a1e3ae92a0529b488f4ed6e
Santiago Ramos <sramos@sitiodistinto.net>
2011-03-13 13:29:25 +0000

    Bugs menores

--- db/migrate/20110311113153_create_pagos.rb
+++ db/migrate/20110311113153_create_pagos.rb
@@ -1,7 +1,7 @@
 class CreatePagos < ActiveRecord::Migration
   def self.up
     create_table :pagos do |t|
-      t.decimal :importe, :null => false
+      t.decimal :importe, :precision => 8, :scale => 2, :null => false
       t.date :fecha, :null => false
       t.integer :factura_id
 
