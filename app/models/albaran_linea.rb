b62573113027ca15010ddff9aa175165b6200cbe
Santiago Ramos <sramos@sitiodistinto.net>
2011-04-01 09:10:31 +0000

    Retoques en listados de lineas de albaran (correccion de iva)

--- app/models/albaran_linea.rb
+++ app/models/albaran_linea.rb
@@ -9,7 +9,7 @@ class AlbaranLinea < ActiveRecord::Base
       precio = self.precio_compra * self.cantidad * (1 - self.descuento.to_f/100)
       #precio = self.precio_compra * (1 - self.producto.familia.iva.valor.to_f/100)
     else
-      precio = self.producto.precio * self.cantidad * (1 - self.descuento.to_f/100) * (1 - self.producto.familia.iva.valor.to_f/100)
+      precio = self.producto.precio * self.cantidad * (1 - self.descuento.to_f/100) / (1 + self.producto.familia.iva.valor.to_f/100)
     end
     return precio 
   end
