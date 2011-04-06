79e04d225901d59d47e85cce8b028f4e0390bfc6
Santiago Ramos <sramos@sitiodistinto.net>
2011-04-06 17:29:21 +0000

    Error #7 . Retocando campos

--- db/migrate/20110401135022_create_sessions.rb
+++ db/migrate/20110401135022_create_sessions.rb
@@ -1,5 +1,6 @@
 class CreateSessions < ActiveRecord::Migration
   def self.up
+    drop_table :sessions
     create_table :sessions do |t|
       t.string :session_id, :null => false
       t.text :data
