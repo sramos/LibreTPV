0e5e43057bc0ef0051bc38ab2c61614b6f2ba97b
Santiago Ramos <sramos@sitiodistinto.net>
2011-04-18 13:53:27 +0000

    Volviendo al seed original

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,21 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 
-#Iva.create([		{ :nombre => 'Superreducido', :valor => 4},
-#			{ :nombre => 'Reducido', :valor => 8},
-#			{ :nombre => 'General', :valor => 18} ])
-#
-#Familia.create([	{ :nombre => 'Libros', :iva => Iva.find_by_nombre('Superreducido') },
-#			{ :nombre => 'Entradas', :iva => Iva.find_by_nombre('Reducido') } ])	
-#
-#FormaPago.create([	{ :nombre => 'Efectivo', :caja => true}, { :nombre => 'Tarjeta' }, { :nombre => 'Transferencia' }, { :nombre => 'Domiciliacion' } ])
-#
-#Campo.create([		{ :nombre => 'Autor'}, { :nombre => 'Año'}, { :nombre => 'Editor'}, { :nombre => 'Coleccion'},
-#			{ :nombre => 'Descripcion'}  ])
-#
-#Proveedor.create([	{ :nombre => 'Timadores Sin Fronteras', :cif => 'A-88554345-J', :descuento => '30' } ] )
-#
-#Cliente.create([	{ :nombre => 'Caja', :cif => 'N/A' } ] )
+Iva.create([		{ :nombre => 'Superreducido', :valor => 4},
+			{ :nombre => 'Reducido', :valor => 8},
+			{ :nombre => 'General', :valor => 18} ])
+
+Familia.create([	{ :nombre => 'Libros', :iva => Iva.find_by_nombre('Superreducido') },
+			{ :nombre => 'Entradas', :iva => Iva.find_by_nombre('Reducido') } ])	
+
+FormaPago.create([	{ :nombre => 'Efectivo', :caja => true}, { :nombre => 'Tarjeta' }, { :nombre => 'Transferencia' }, { :nombre => 'Domiciliacion' } ])
+
+Campo.create([		{ :nombre => 'Autor'}, { :nombre => 'Año'}, { :nombre => 'Editor'}, { :nombre => 'Coleccion'},
+			{ :nombre => 'Descripcion'}  ])
+
+Proveedor.create([	{ :nombre => 'Timadores Sin Fronteras', :cif => 'A-88554345-J', :descuento => '30' } ] )
+
+Cliente.create([	{ :nombre => 'Caja', :cif => 'N/A' } ] )
 
 Configuracion.create([	{ :nombre_param => 'PAGINADO', :valor_param => '25'},
 			{ :nombre_param => 'NOMBRE CORTO EMPRESA', :valor_param => 'Librería Ejemplo'},
