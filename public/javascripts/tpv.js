84d61f96c2adbe90cdd1091db6667019d8bb8970
Santiago Ramos <sramos@sitiodistinto.net>
2011-05-02 19:02:29 +0000

    Ticket #18 . Volver a activar ISBN como campo primero

--- public/javascripts/tpv.js
+++ public/javascripts/tpv.js
@@ -1,7 +1,7 @@
 function stopRKey(evt) {
   var evt = (evt) ? evt : ((event) ? event : null);
   var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
-  if ((evt.keyCode == 13) && (node.id=="formulario_campo_codigo"))  { 
+  if ((evt.keyCode == 13) && (node.id=="formulario_campo_producto_codigo"))  { 
     return false;
   }
 }
