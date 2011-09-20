f98746e7288225c93f0ec3b74caf5edbd336a653
Santiago Ramos <sramos@sitiodistinto.net>
2011-09-20 21:59:01 +0200

    Customers and providers should not be erased if they have invoices

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,7 +1,14 @@
 class Cliente < ActiveRecord::Base
 
+  has_many :albarans
+
   validates_presence_of :nombre, :cif
 
-  has_many :albarans
+  before_destroy :verificar_borrado
+
+  def verificar_borrado
+    errors.add("Albaran", "El cliente no se puede borrar. Tiene asociados albaranes") unless self.albarans.empty?
+    return false unless errors.empty?
+  end
   
 end
--- app/models/proveedor.rb
+++ app/models/proveedor.rb
@@ -1,8 +1,16 @@
 class Proveedor < ActiveRecord::Base
 
-  validates_presence_of :nombre, :cif
-
   has_many :albarans
   has_many :facturas
+
+  validates_presence_of :nombre, :cif
+
+  before_destroy :verificar_borrado
+
+  def verificar_borrado
+    errors.add("Albaran", "El proveedor no se puede borrar. Tiene asociados albaranes") unless self.albarans.empty?
+    errors.add("Factura", "El proveedor no se puede borrar. Tiene asociadas facturas") unless self.facturas.empty?
+    return false unless errors.empty?
+  end
 
 end
