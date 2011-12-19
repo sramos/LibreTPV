50a5ae44868da04ca816ae3bf41f07e0f9e9582b
Santiago Ramos <sramos@sitiodistinto.net>
2011-12-19 21:18:47 +0100

    Invoices scheduller

--- config/initializers/scheduller.rb
+++ config/initializers/scheduller.rb
@@ -5,3 +5,25 @@ scheduler = Rufus::Scheduler.start_new
 #  Test.check_thumbnails!
 #end
 
+# Comprueba las facturas
+scheduler.every("1m") do
+  Factura.vencidas.each do |factura|
+    diferencia = (factura.fecha_vencimiento - Time.now.to_date).abs.to_s
+    valores = { :objeto => "Factura", :objeto_id => factura.id, :url => "/productos/factura/listado" }
+    aviso = Avisos.first(:conditions => valores )
+    aviso ||= Avisos.new(valores)
+    aviso.mensaje = "Hay una factura vencida hace " + diferencia + " días, que aún no está pagada!"
+    aviso.criticidad = 3
+    aviso.save
+  end
+  Factura.proximos_vencimientos.each do |factura|
+    diferencia = (Time.now.to_date - factura.fecha_vencimiento).abs.to_s
+    valores = { :objeto => "Factura", :objeto_id => factura.id, :url => "/productos/factura/listado" }
+    aviso = Avisos.first(:conditions => valores )
+    aviso ||= Avisos.new(valores)
+    aviso.mensaje = "Una factura vencerá en " + diferencia + " días"
+    aviso.criticidad = 2
+    aviso.save
+  end
+end
+
