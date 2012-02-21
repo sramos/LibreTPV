864641e93247ab06c2a4de65c4e19957d56a53c9
Santiago Ramos <sramos@sitiodistinto.net>
2012-02-21 20:40:57 +0100

    init scheduller breaks db migration

--- config/initializers/scheduller.rb
+++ config/initializers/scheduller.rb
@@ -6,10 +6,10 @@ scheduler = Rufus::Scheduler.start_new
 #end
 
 # Chequeo en el arranque
-Avisos.depositos_no_devueltos
-Avisos.proxima_devolucion_depositos
-Avisos.proximos_vencimientos_facturas
-Avisos.facturas_vencidas
+#Avisos.depositos_no_devueltos
+#Avisos.proxima_devolucion_depositos
+#Avisos.proximos_vencimientos_facturas
+#Avisos.facturas_vencidas
 
 # Comprueba las facturas
 scheduler.every("6h") do
