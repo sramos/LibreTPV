66156efc0c749b10e70b4dd7b5a2914a02f7d15c
Santiago Ramos <sramos@sitiodistinto.net>
2012-06-26 22:26:39 +0200

    Bug in migrates sequence

--- db/migrate/20120601165409_add_last_sell_code_to_configuration.rb
+++ db/migrate/20120601165409_add_last_sell_code_to_configuration.rb
@@ -1,7 +1,7 @@
 class AddLastSellCodeToConfiguration < ActiveRecord::Migration
   def self.up
     add_column :configuracion, :editable, :boolean, :default => true
-    ultima_factura = Factura.count(:include => ["albaran"], :conditions => ["albarans.cliente_id IS NOT NULL"])
+    ultima_factura = Factura.count(:include => ["albarans"], :conditions => ["albarans.cliente_id IS NOT NULL"])
     Configuracion.create(:nombre_param => "FACTURAS VENTA", :valor_param => ultima_factura.to_s, :editable => false)
     impresion = Configuracion.find_by_nombre_param("COMANDO IMPRESION")
     impresion.editable = false
