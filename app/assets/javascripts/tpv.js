5f4280df72e83a48b824eb845d4652347e6192bd
sramos <sramos@sitiodistinto.net>
2019-01-29 19:40:24 +0100

    Secrets

--- app/assets/javascripts/tpv.js
+++ app/assets/javascripts/tpv.js
@@ -2,7 +2,7 @@
 function stopRKey(evt) {
   var evt = (evt) ? evt : ((event) ? event : null);
   var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
-  if ((evt.keyCode == 13) && (node.id=="formulario_campo_producto_codigo"))  { 
+  if ((evt.keyCode == 13) && (node.id=="formulario_campo_producto_codigo"))  {
     return false;
   }
 }
@@ -16,4 +16,3 @@ Event.observe(window, 'load', function() {
 function fadeout(){
     new Effect.Opacity("mensaje", {duration:1.5, from:1.0, to:0.0});
 }
-
